3bfcf7a18016be24f75db30145b3af31699ac816
cesar91bo <cesar91bo@gmail.com>
2025-07-21 20:31:54 -0300

    Evitgando duplicados de ordenes

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,9 +4,21 @@ export default function useOrder(){
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item: MenuItem) => {
-        
-        const newItem = {...item, quantity: 1}
-        setOrder([...order, newItem]) 
+        //Validar que exista el item en el pedido
+        const existingItem = order.find(orderItem => orderItem.id === item.id)
+        if(existingItem){
+            //Si existe, incrementar la cantidad
+            const updatedOrder = order.map(orderItem => 
+                orderItem.id === item.id 
+                ? {...orderItem, quantity: orderItem.quantity + 1} 
+                : orderItem
+            )
+            setOrder(updatedOrder)
+        }
+        else{
+            const newItem = {...item, quantity: 1}
+            setOrder([...order, newItem]) 
+        }
     }
 
     return{
