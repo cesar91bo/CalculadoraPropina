913d6eccf9fc4d212be77538cb23b362a0b74ba7
cesar91bo <cesar91bo@gmail.com>
2025-07-25 19:37:03 -0300

    se creó nuevo componente para cargar los consumos y la cantidad

---
+++ src/components/OrderContents.tsx
@@ -0,0 +1,23 @@
+import { OrderItem } from "../types/";
+type OrderContentsProps = {
+    order: OrderItem[];
+} 
+
+export default function OrderContents ({ order }: OrderContentsProps) {
+  return (
+    <div>
+        <h2 className='font-black text-4xl'>Consumo</h2>
+        <div className="space-y-3 mt-5">
+          {order.length === 0 ? 
+          <p className="text-center">La orden esta vacío</p>
+        : (order.map(item => (
+            <div key={item.id} className="flex justify-between items-center">
+              <p>{item.name}</p>
+              <p>{item.quantity}</p>
+            </div>
+            ))    
+          )}
+        </div>
+    </div>
+  )
+}
