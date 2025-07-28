86a595c451e18c003194c5658e93d7fc160d4069
cesar91bo <cesar91bo@gmail.com>
2025-07-27 21:41:00 -0300

    se actualizó el hook para agregar la funcionalidad para eliminar un item y se aplicó la lógica

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -21,8 +21,13 @@ export default function useOrder(){
         }
     }
 
+    const removeItem = (id: MenuItem['id']) => {
+        //Eliminar un item del pedido
+        setOrder(order.filter(item => item.id !== id))
+    }
     return{
         order,
-        addItem
+        addItem,
+        removeItem
     }
 }
